d8a92267465a981f0de47836653ce0c6b4b5831e
omtekyav <174030142+omtekyav@users.noreply.github.com>
2025-12-04 22:53:09 +0300

    feat : custom validator added hybrid approach being used

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field , field_validator
+import re
 
 #giriş modeli (REQUEST)
 
@@ -14,12 +15,25 @@ class SentimentRequest(BaseModel):
     
 #çıkış model(Response)
     @field_validator('text')
-    @classmethod
+    #validasyon olduğu için classmethod olarak kullanıyoruz
+    @classmethod # daha ortada üretilmiş bir response olmadığı için classmethod'dan cagırıyoruz yani instance yok ortada ürün yok
 
-    def check_not_empty(cls,v:str): #ilk parametre sınıfın kendisi olacak v : value > bunlar
+    def check_valid_text(cls,v:str): #ilk parametre sınıfın kendisi olacak v : value > bunlar
         if not v.strip():
             raise ValueError('Metin boşluklardan oluşamaz anlamlı bir metin giriniz.')
+        
+
+
+
+        if not re.search(r'[a-zA-Z]', v): #kücük harf veya büyük harf
+            raise ValueError('Metin en az bir harf içermelidir(sadece sayı ve semboller kabul edilmez)')   
+        
+
         return v
+    
+
+
+
 
 
 
--- src/services.py
+++ src/services.py
@@ -12,14 +12,31 @@ class SentimentService:
     def __init__(self):
         self.interactor = SentimentModelInteractor()
 
+        self.negative_keywords = {"kötü", "berbat", "çöp", "beğenmedim", "pişman", "problem", "bozuk", "hata", "memnun değilim", "tavsiye etmem"}
+        
+
+
     def analyze_text(self, text:str) ->SentimentResponse:
         clean_text = text.strip().lower()
 
+        for word in clean_text.split():
+            if word in self.negative_keywords:
+                print(f"LOG:kural tabanlı yasak kelime bulundu! Yasaklı kelime:'{word}'")
+
+                return SentimentResponse(
+
+                    sentiment = "Negatif",
+                    confidence= 1.0 #kesinlik var çünkü kelime sınırına takıldı
+                )
+
+        #klasik yapay zeka kontrolü
+
+        #makiniye sorulacak kısım self.interactor'dan predict cleantext'i tahmin edecek ve sonuc döndürecek
         prediction = self.interactor.predict(clean_text) # zor iş tahmin işini interactore devrettik
 
         return SentimentResponse(
 
-        sentiment = prediction["label"],
+        sentiment = prediction["label"],    
         confidence= prediction["score"],
 
     )
